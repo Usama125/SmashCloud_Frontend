6255662ca32ee41166c526645f7778478487e8ab
Usama Farooq <usama.farooq@innroad.com>
2022-07-26 14:31:24 -0700

    Live URL Updated

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8080/api'
+  baseURL: 'https://lit-cove-57081.herokuapp.com/api'
 })
 
 export default instance  
